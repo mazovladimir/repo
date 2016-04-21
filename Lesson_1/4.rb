0be7f010eaf4f9fb6a24dd8d41295742abfaf568
root <root@spacewalk.ruzik.net>
2016-04-21 23:00:21 +0300

    Lesson_1.5

--- Lesson_1/4.rb
+++ Lesson_1/4.rb
@@ -8,16 +8,12 @@ c = gets.to_f
 D = b**2 - 4*a*c
 
 if D < 0 
-    puts "D=#{D}, Korney net"
-end
-
-if D > 0 
+  puts "D=#{D}, Korney net"
+elsif D > 0 
   x1 = (-b + Math.sqrt(D)) / (2*a) 
   x2 = (-b - Math.sqrt(D)) / (2*a)
   puts "D=#{D}, x1 = #{x1}, x2 = #{x2}"
-end
-
-if D == 0
+else
   x = -b/(2*a)
   puts "D=#{D}, x = #{x}"
 end
