58d2c1c964e07af26adfba771691b20e68dfa5af
root <root@spacewalk.ruzik.net>
2016-04-23 18:57:05 +0300

    Lesson_2.1

--- Lesson_2/6.rb
+++ Lesson_2/6.rb
@@ -1,23 +1,31 @@
+a = {}
+obshee = 0
+
 loop do
 
 print "Vvedite nazvanie tovara: "
 name = gets.chomp
 
-break if name == "stop"
-
+if name == "stop"
+  break
+end
+  
 print "Vvedite cenu za tovar: "
 price = gets.to_i
 
 print "Vvedite kolichestvo tovara: "
 count = gets.to_f
 
-a = {}
-
 a[name] = {price => count}
-total = count * price
-
 end
 
-puts "#{a}"
-puts "#{name} stoit - #{total}"
+puts a
+a.each do |key1,value1| 
+  value1.each do |key2,value2|
+    total = key2 * value2    
+    obshee += total
+    puts "#{key1} - stoit #{total}"
+  end
+end
 
+puts "Polnaya Stoimost v korzine - #{obshee}"
