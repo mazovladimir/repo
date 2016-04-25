7256e4709390a8ff06949ccfbcade08892927bb4
root <root@spacewalk.ruzik.net>
2016-04-25 13:35:31 +0300

    Lesson_2.2

--- Lesson_2/5.rb
+++ Lesson_2/5.rb
@@ -1,11 +1,24 @@
 print "Enter the date: "
-date = gets
+date = gets.to_i
 
 print "Enter the month: "
-month = gets
+month = gets.to_i
 
 print "Enter the year: "
-year = gets
+year = gets.to_i
 
-months = {"January" => 31, "Febraury" => 28, "March" => 31, "April" => 30, "May" => 31, "June" => 30, "July" => 31, "August" =>31, "September" => 30, "October" => 31, "November" => 30, "December" => 31}
-months.each {|key,value| puts "#{key}" if value == 30 }
+all_day = 0
+
+months = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+
+if (year==2000) || ((year%4==0 || year%400==0) && (year%100!=0))
+  months[1] = 29
+  puts "God visokosniy"
+end
+
+months.each_index do |day|
+  all_day += months[day] if (day + 1) < month
+end
+
+all_day += date
+puts all_day
