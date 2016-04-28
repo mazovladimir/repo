a693cded4f98e2e74cf23cd655ac39e8499e8c30
root <root@spacewalk.ruzik.net>
2016-04-28 18:39:57 +0300

    Lesson_3.2

--- Lesson_3/train.rb
+++ Lesson_3/train.rb
@@ -33,15 +33,20 @@ class Route
   def delete_station(del_st)
     @station.delete(del_st)
   end
+
+  def show_station
+    @station
+  end
 end
 
 class Train < Route
-  attr_reader :speed, :vagon
+  attr_reader :speed, :vagon, :route
   def initialize(number,type,vagon)
     @number = number
     @type = type
     @vagon = vagon
     @speed = 0
+    @index_station = 0
   end
 
   def increase_speed
@@ -62,5 +67,23 @@ class Train < Route
     else
       puts "Vagonov net"
     end
+  end
+
+  def route_inherit(path)
+    @route = path.station
+  end
+
+  def whatis_my_station
+    puts "Before station is: #{@route[@index_station-1]}" if @index_station > 0
+    puts "Current station is: #{@route[@index_station]}"
+    puts "Next station is: #{@route[@index_station+1]}"
+  end
+
+  def next_station
+    @index_station += 1 if @index_station < @route.count - 1
+  end
+
+  def before_station
+    @index_station -=1 if @index_station > 0 
   end
 end
