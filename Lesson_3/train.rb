a0231edd56ef518fbb7c757aba27c1cf77572bd2
root <root@spacewalk.ruzik.net>
2016-05-03 16:34:42 +0300

    Lesson_3.14

--- Lesson_3/train.rb
+++ Lesson_3/train.rb
@@ -72,19 +72,11 @@ class Train
     @index_station = 0
   end
 
-  def increase_speed
-    @speed += 10
-  end
-
-  def break_speed
-    @speed = 0
-  end
-
   def vagon_detach
-    if @speed==0 && @vagon>0
+    if @speed==0 && @vagon>0 && @type == "passanger"
       @vagon-=1
-    else
-      puts "Vagonov net"
+    elsif @speed==0 && @vagon>0 && @type == "cargo"
+      @vagon-=1
     end
   end
 
@@ -93,13 +85,25 @@ class Train
   end
 
   def vagon_attach
-    @vagon+=1
+    if @type == "passanger" && @speed == 0
+      @vagon+=1
+    elsif @type == "cargo" && @speed == 0
+      @vagon+=1
+    end
   end
 
   def whatis_my_station
     puts "Before station is: #{@route[@index_station-1]}" if @index_station > 0
     puts "Current station is: #{@route[@index_station]}"
     puts "Next station is: #{@route[@index_station+1]}"
+  end
+
+  def increase_speed
+    @speed += 10
+  end
+
+  def break_speed
+    @speed = 0
   end
 
   def next_station
@@ -112,19 +116,13 @@ class Train
 end
 
 class PassangerTrain < Train
-end
-
-class CargoTrain < Train
-end
-
-class PassangerVagon < Train
-  def initialize
-  end
-
   def vagon_attach
     super
   end
 end
-  
-class CargoVagon < Train
+
+class CargoTrain < Train
+  def vagon_attach
+    super
+  end
 end
